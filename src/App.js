b74555efa1eefdf513e86f2203c861317e63211c
Ricardo Soto-Fabela <sotofabela@gmail.com>
2022-11-08 19:32:51 -0600

    fix error

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import Header from './Header.js';
 import Main from "./Main.js";
-import HornedBeast from './HornedBeast.js';
 import Footer from "./Footer.js";
 import './App.css';
 
